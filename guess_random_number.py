1420f6c269767828fbc94a7f9d34b3aca465dd3c
Guru Murthy C L <clguru.murthy007@gmail.com>
2022-04-15 20:30:32 +0530

    Updated the guess number program

--- guess_random_number.py
+++ guess_random_number.py
@@ -16,8 +16,33 @@ def guess(x):
     print(f"Correct.. You have guessed the number... {random_number}")
 
 
+
+def computer_gues(x):
+    low = 1
+    high = x
+    feedback = ''
+    
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+
+        feedback = input(f"Is the number {guess} is High [h] Or Low [l] Or Correct [c] ? ")
+
+        if feedback == 'l':
+            low = guess+1
+            guess = random.randint(low, high)
+
+        elif feedback == 'h':
+            high = guess-1 
+            guess = random.randint(low, high)
+
+    print(f"Yay... Computer has guessed the number... {guess}")
+
+
 if __name__ == "__main__":
-    guess(10)
+    computer_gues(1000)
 
 
 
--- word_counts.py
+++ word_counts.py
@@ -1,9 +1,28 @@
+import collections
+
+def word_count(file_path, word):
+    file_content = open(file_path, 'r')
+    lines_list = file_content.read()
+    words_list = lines_list.split()
+    trimmed_words_list = list(map(lambda x: x.lower().replace(',','').replace('.','').replace('\'',''), words_list))
+    words_count_list = collections.Counter(trimmed_words_list)
+    print(type(words_count_list))
+    print(words_count_list)
+    file_name = file_path.split("/")[-1]
+    print(f"word {word} is found {words_count_list[word]} times in the file {file_name}")
+
+if __name__=='__main__':
+    file_path = input("Enter the file path to do word count: ")
+    word = input("Enter the word to search on the file: ")
+    word_count(file_path, word)
 
 
-
-
-file_content = open('/Users/gurumurthy.cl/Documents/python_tutorial/resources/text.txt', 'r')
-lines = file_content.readlines()
-
-for line in lines:
-    print(line.split(' ')[0].lower().replace(',',''))
+# filtered_words_list = list(filter(lambda x: x=='rakuten', trimmed_words_list ))
+#
+# print(f"Number of words in the file is: {len(trimmed_words_list)}")
+#
+# count = 0
+# for line in lines_list:
+#     for words in line.split():
+#         if (words.lower().replace('.','')=='rakuten'):
+#             count = count + 1
