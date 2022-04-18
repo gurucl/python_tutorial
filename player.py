92a6854a554c6731f8aa6ebafaa09d5b182ad706
Guru Murthy C L <clguru.murthy007@gmail.com>
2022-04-18 10:57:15 +0530

    Added files for TicTacToe game

---
+++ player.py
@@ -0,0 +1,45 @@
+import random
+
+
+class Player:
+    def __init__(self, letter) -> None:
+        # letter X or O
+        self.letter = letter
+
+
+    # We want all players to get their next move 
+    def get_move(self, game):
+        pass
+
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter) -> None:
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+
+
+
+class HumanPlayer(Player):
+    def __init__(self, letter) -> None:
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(f"{self.letter}'s turn. Input move (0-8)")
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print("Invalid square. Try again")
+
+        return val
+
+
+
